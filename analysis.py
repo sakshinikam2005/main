18d254d0638a55a2cb46f886ae98b0cd18b08f26
sakshinikam2005 <sakshi.nikam.aids.2023@vpkbiet.org>
2026-02-03 16:05:44 +0530

    Add run_analysis function

--- analysis.py
+++ analysis.py
@@ -2,3 +2,8 @@ def clean_data(data):
     data = data.dropna()
     data = data.drop_duplicates()
     return data
+
+def main():
+    print("Starting analysis pipeline...")
+def run_analysis():
+    print("Initializing analysis system...")
